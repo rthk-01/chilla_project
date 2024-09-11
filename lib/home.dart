24feb6b1ee6adb11f34559e052abdde9912f86db
Rithik B J <146831067+rthk-01@users.noreply.github.com>
2024-09-11 20:16:06 +0530

    scrollable homepage

--- lib/home.dart
+++ lib/home.dart
@@ -38,83 +38,87 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.5,
-            width: MediaQuery.of(context).size.width,
-            child: Stack(
-              children: [
-                AnimatedSwitcher(
-                  duration: Duration(seconds: 3),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.zero,
-                    child: Image.asset(
-                      images[_currentIndex],
-                      key: ValueKey(_currentIndex),
-                      fit: BoxFit.cover,
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.5,
+    return SingleChildScrollView(
+      physics: ClampingScrollPhysics(),
+      child: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  AnimatedSwitcher(
+                    duration: Duration(seconds: 3),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.zero,
+                      child: Image.asset(
+                        images[_currentIndex],
+                        key: ValueKey(_currentIndex),
+                        fit: BoxFit.cover,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height * 0.5,
+                      ),
                     ),
+                    transitionBuilder:
+                        (Widget child, Animation<double> animation) {
+                      return FadeTransition(
+                        opacity: animation,
+                        child: child,
+                      );
+                    },
                   ),
-                  transitionBuilder:
-                      (Widget child, Animation<double> animation) {
-                    return FadeTransition(
-                      opacity: animation,
-                      child: child,
-                    );
-                  },
-                ),
-                Positioned(
-                  right: 0,
-                  height: 10,
-                  child: ClipRRect(
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 0, sigmaY: 10),
-                      child: Container(
-                        color: Colors.white,
+                  Positioned(
+                    right: 0,
+                    height: 10,
+                    child: ClipRRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 0, sigmaY: 10),
+                        child: Container(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 0.04 * MediaQuery.of(context).size.height),
-          Container(
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                Container(
-                  child: Image.asset('assets/chilla_image.png'),
-                ),
-                SizedBox(height: 0.04 * MediaQuery.of(context).size.height),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Get Started',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+            SizedBox(height: 0.04 * MediaQuery.of(context).size.height),
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Container(
+                    child: Image.asset('assets/chilla_image.png'),
+                  ),
+                  SizedBox(height: 0.04 * MediaQuery.of(context).size.height),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(190, 52),
+                      backgroundColor: Color(0xFF57CC99),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      shadowColor: Colors.black,
+                      elevation: 9,
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(190, 52),
-                    backgroundColor: Color(0xFF57CC99),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    shadowColor: Colors.black,
-                    elevation: 9,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 0.04 * MediaQuery.of(context).size.height),
+          ],
+        ),
       ),
     );
   }
